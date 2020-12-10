576a6ef57ef5b85ed563735234438106eb10ea21
Stagner <75423387+Stagner@users.noreply.github.com>
2020-12-10 14:13:00 +0400

    Create case.py

---
+++ case.py
@@ -0,0 +1,20 @@
+class Case:
+    def __init__(self,terrain,lemming=None):
+        self.ter=terrain #    
+        self.lem=lemming #on set le truc 
+     
+    def __libre(self):
+        if self.ter==None:
+            return False
+        else:
+            return True
+    
+    def depart(self):
+        entree=
+    
+    def arrivee(self,lem):
+        pass
+    
+    def __str__(self):
+        pass
+    
